b5fa0f0c7fd10068368da472652e1af70284d5ad
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-12 15:26:37 +0930

    Update function-1-1.cpp

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
-
 using namespace std;
-
-void print_matrix(int array[10][10]){
-  
-   for(int i=0, i<10, i++){
-  
-  	for(int j=0, j<10, j++){
-      
-       cout<<arr[i][j]<<" ";
+void printer(int array[10][10])
+{
+	//printing the array
+  for(int i=0;i<10;i++)
+  {
+  	for(int j=0;j<10;j++)
+  	{
+       cout<<array[i][j]<<" ";
   	}
+  	
   	cout<<endl;
   }
 
+}
+int main()
+{
+  int array[10][10];
+  for(int i=0;i<10;i++)
+  {
+  	for(int j=0;j<10;j++)
+  	{
+  		cin>>array[i][j];
+  	}
+  }
+  printer(array);
 }
