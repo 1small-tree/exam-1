077dc5d231855b9cc0f299fbcd18dd4bb4b280c6
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-12 15:07:32 +0930

    Create main-2-2.cpp

---
+++ main-2-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main{
+  
+  int binary_digits[]；
+  
+  int number_of_digits；
+  
+    cout<<binary_digits[]<<endl;
+  
+  cout<<number_of_digits<<endl;
+  
+  return 0;
+}
